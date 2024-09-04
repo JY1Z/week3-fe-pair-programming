0c180da53c93b9c9dfa68957cfa2094ae7116dee
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-04 11:13:51 +0300

    Update project

---
+++ src/components/Tours.js
@@ -0,0 +1,25 @@
+import Tour from './Tour';
+import { tours } from '../data';
+
+const Tours = () => {
+  return (
+    <>
+      {/* Tours section starts */}
+      <section className="section" id="tours">
+        <div className="section-title">
+          <h2>
+            featured <span>tours</span>
+          </h2>
+        </div>
+        <div className="section-center featured-center">
+          {tours.map((tour) => (
+            <Tour key={tour.id} {...tour} />
+          ))}
+        </div>
+      </section>
+      {/* Tours section ends */}
+    </>
+  );
+}
+
+export default Tours;
